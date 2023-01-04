a4f44400f16f0753dfe092a30485704206dc9b1e
Fabio Oshiro <fabio.oshiro@gmail.com>
2023-01-03 23:48:48 -0300

    new executor \o/

--- token-metadata/program/src/main.rs
+++ token-metadata/program/src/main.rs
@@ -1,19 +1,15 @@
+use cartesi_solana::anchor_lang::solana_program::entrypoint::ProgramResult;
+use cartesi_solana::executor::create_executor;
+
+use mpl_token_metadata::{error::MetadataError, processor};
+use solana_program::{account_info::AccountInfo, program_error::PrintProgramError, pubkey::Pubkey};
 pub(crate) use std::io;
 
-use cartesi_solana::{adapter::{get_processor_args, persist_accounts}, cartesi_stub::CartesiStubs};
-use mpl_token_metadata::{processor, error::MetadataError};
-use solana_program::{pubkey::Pubkey, account_info::AccountInfo, program_error::PrintProgramError};
-use cartesi_solana::anchor_lang::solana_program::entrypoint::ProgramResult;
-
 fn main() -> io::Result<()> {
-    let (program_id, accounts, data, delete) = get_processor_args();
-    
-    solana_program::program_stubs::set_syscall_stubs(Box::new(CartesiStubs { program_id: program_id.clone() }));
-
-    process_instruction(&program_id, &accounts, &data).unwrap();
-    
-    println!("Persist token-metadata accounts...");
-    persist_accounts(&accounts, delete);
+    let mut executor = create_executor();
+    executor.get_processor_args(|program_id, accounts, data| {
+        process_instruction(&program_id, accounts, &data).unwrap();
+    });
     Ok(())
 }
 
@@ -22,7 +18,6 @@ fn process_instruction<'a>(
     accounts: &'a [AccountInfo<'a>],
     instruction_data: &[u8],
 ) -> ProgramResult {
-
     if let Err(error) = processor::process_instruction(program_id, accounts, instruction_data) {
         // catch the error so we can print it
         error.print::<MetadataError>();
