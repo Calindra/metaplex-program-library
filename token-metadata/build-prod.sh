1e6f44a330d06e77d28db4b574f20748691b9aad
Fabio Oshiro <fabio.oshiro@gmail.com>
2023-01-06 12:30:31 -0300

    fix: cp the bin file to solana-adapter folder

--- token-metadata/build-prod.sh
+++ token-metadata/build-prod.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
 
-rm -rf ./target
+set -e
+
 cargo build -Z build-std=std,core,alloc,panic_abort,proc_macro --target ./riscv64ima-cartesi-linux-gnu.json --release
-
+cp ./target/debug/mpl-token-metadata ../../rollups-examples/solana-adapter/solana_smart_contract_bin/metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s
+cargo clean
 echo "done."
